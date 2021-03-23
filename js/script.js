4356699a27c99fb505103dc599cbc120851aaa20
Carnicero90 <filippomontani82@gmail.com>
2021-03-23 15:50:47 +0100

    aggiunta funzia bonus per riordinare un'array (senza sort)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 var fizz = 0;
 var buzz = 0;
 var fizzbuzz = 0;
+var range = 100;
 
-for (var counter = 1; counter <= 100; counter++) {
+for (var counter = 1; counter <= range; counter++) {
     var num = "";
     if (counter % 3 == 0) {
         num += "Fizz";
@@ -25,3 +26,37 @@ document.getElementById("fizzbuzz").innerHTML = fizzbuzz;
 
 /* purtroppo lo avevo già fatto in js qualche giorno fa,
     lo stesso identico esercizio */
+
+
+// BONUS
+var arr = [];
+for (var i = 0; i < 10; i++) {
+    arr.push(Math.floor(Math.random() * 100) + 1);
+}
+console.log("non ordinata: ", arr);
+for (var i = 0; i < arr.length; i++) {
+    for (var z = 0; z < i; z++) {
+        var num = arr[i];
+        if (num < arr[z]) {
+            var num2 = arr[z];
+            arr[i] = num2;
+            arr[z] = num;
+        }
+    }
+}
+console.log("ordinata: ", arr);
+
+
+
+// function reorder(array) {
+//     for (var i = 0; i < array.length; i++) {
+//         for (var z = 0; z < i; z++) {
+//             var num = array[i];
+//             if (num < array[z]) {
+//                 var num2 = array[z];
+//                 array[i] = num2;
+//                 array[z] = num;
+//             }
+//         }
+//     }
+// }
