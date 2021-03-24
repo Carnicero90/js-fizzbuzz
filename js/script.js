7b60098cd19a40d9126179e3eef5851979e1303f
Carnicero90 <filippomontani82@gmail.com>
2021-03-24 08:46:50 +0100

    validato, aggiunti vendor-prefix a css/style.css via autoprefixer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 var fizz = 3;
 var buzz = 5;
-var fizzbuzz = fizz * buzz;
+// var fizzbuzz = fizz * buzz;
 var range = 100;
+var totFizz = (range - range % fizz) / fizz;
+/* a titolo esemplificativo: (100 - 100 % 3) dà come risultato l'ultimo multiplo
+di 3 inferiore a 100 (100 -1 = 99): dividendolo per 3, ho come risultato il 
+n.ro totale di multipli di 3 inferiori a 100 (99/3 = 33)
+*/
+var totBuzz = (range - range % buzz) / buzz;
+var totFizzbuzz = (totBuzz - totBuzz % fizz) / fizz;
 
 for (var counter = 1; counter <= range; counter++) {
     var num = "";
@@ -15,13 +22,9 @@ for (var counter = 1; counter <= range; counter++) {
     console.log(num || counter)
 }
 
-document.getElementById("fizz").innerHTML = (range - range % fizz) / fizz;
-/* a titolo esemplificativo: (100 - 100 % 3) dà come risultato l'ultimo multiplo
-di 3 inferiore a 100 (100 -1 = 99): dividendolo per 3, ho come risultato il 
-n.ro totale di multipli di 3 inferiori a 100 (99/3 = 33)
-*/
-document.getElementById("buzz").innerHTML = (range - range % buzz) / buzz;
-document.getElementById("fizzbuzz").innerHTML = (range - range % fizzbuzz) / fizzbuzz;
+document.getElementById("fizz").innerHTML = totFizz;
+document.getElementById("buzz").innerHTML = totBuzz;
+document.getElementById("fizzbuzz").innerHTML = totFizzbuzz;
 
 
 /* purtroppo lo avevo già fatto in js qualche giorno fa,
@@ -33,7 +36,7 @@ var arr = [];
 for (var i = 0; i < 10; i++) {
     arr.push(Math.floor(Math.random() * 100) + 1);
 }
-console.log("non ordinata: ", arr);
+console.log("non ordinata:\n" + arr.join("\n"));
 for (var i = 0; i < arr.length; i++) {
     for (var z = 0; z < i; z++) {
         var num = arr[i];
@@ -44,4 +47,4 @@ for (var i = 0; i < arr.length; i++) {
         }
     }
 }
-console.log("ordinata: ", arr);
+console.log("ordinata:\n" + arr.join("\n"));
